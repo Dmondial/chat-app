53d1414bdc22b21b13f92f95abe257520a4c2bc6
Dmondial <www.ezweb.1ocofrank.acidman@gmail.com>
2020-11-10 20:09:16 +0900

    チャットルームの削除機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "rooms#index"
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create, :index] do
+  resources :rooms, only: [:new, :create, :index, :destroy] do
     resources :messages, only: [:index, :create]
   end
-  
+
 end
